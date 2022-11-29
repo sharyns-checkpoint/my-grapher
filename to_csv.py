19aa124d1bd954d429066c3f04af53f277bd28da
Sharyn Sircovich Sassun <sharyns@checkpoint.com>
2022-11-29 17:06:52 +0200

    valid and comprehensive csv is generated

--- to_csv.py
+++ to_csv.py
@@ -1,20 +1,47 @@
 import sys
 import pandas as pd
 
-input_name = sys.argv[1]
-output_name = sys.argv[2]
+def process(input_name: str, output_name: str):
+    
+    try:
+        with open(input_name, encoding='utf-8') as file:
+            df = None
 
-# check args
-if input_name == "" or input_name.isspace():
-    print("Missing first arg: input filename")
-    exit
-elif input_name == "" or input_name.isspace():
-    print("Missing second arg: output filename")
-    exit
+            for line in file.read().splitlines():
+                df1 = pd.read_json(line)
+                df1.drop(['email', 'name'], inplace=True)
+                df1.reset_index()
 
-# process
-with open(input_name, encoding='utf-8') as file:
-    df = pd.read_json(file)
-    print(f'Loaded and finished with file {input_name}')
+                if df is None:
+                    df = df1
+                else:
+                    df = pd.concat([df, df1])
 
-df.to_csv(output_name, encoding='utf-8', index=False)
+            print(f'Loaded and finished with file {input_name}')
+            print('HEAD:\n')
+            df.head()
+            print('All done')
+    except Exception as ex:
+        print('Error while processing file: ' + str(ex))
+        return
+
+    df = df[['topic', 'number', 'createdOn', 'lastUpdated']]  # filter columns
+    df.to_csv(output_name, encoding='utf-8', index=False)
+
+
+def main():
+    input_name = sys.argv[1]
+    output_name = sys.argv[2]
+
+    # check args
+    if input_name == "" or input_name.isspace():
+        print("Missing first arg: input filename")
+        exit
+    elif input_name == "" or input_name.isspace():
+        print("Missing second arg: output filename")
+        exit
+
+    process(input_name, output_name)
+
+if __name__ == '__main__':
+    main()
