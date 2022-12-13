85a74fe91790d12829b7b39b665a10b730c0ec96
Sharyn Sircovich Sassun <sharyns@checkpoint.com>
2022-12-13 15:50:47 +0200

    better and more beautiful drawer

--- drawer.py
+++ drawer.py
@@ -10,9 +10,13 @@ df = pd.read_csv(filename)
 x = df['date']
 y = df['num']
 
+plt.figure(figsize=(12, 8))
 plt.plot(df['date'], df['num'])
-plt.title('merged commits by ' + username)
+plt.title('merged commits by ' + username, pad=20)
 plt.xticks(x[::5], rotation='vertical')
-plt.xlabel('Day')
-plt.ylabel('# of commits')
+plt.xlabel('day', labelpad=30)
+plt.ylabel('# of commits', labelpad=25)
+#plt.savefig(username + '.pdf', format='pdf')
+plt.tight_layout()
+plt.subplots_adjust(left=0.076, bottom=0.2, right=0.95, top=0.92)
 plt.show()
